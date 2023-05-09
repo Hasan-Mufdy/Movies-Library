eb3e783689d64c2c83c8437cdd7b717a1ccdb532
Hasan-Mufdy <h.mufdy@gmail.com>
2023-05-09 16:33:31 +0000

    added update, delete, and get request functions to the /getmovies page

--- schema.sql
+++ schema.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE IF NOT EXISTS movies (
     movieID SERIAL PRIMARY KEY,
     movieName VARCHAR(255),
-    movieInfo VARCHAR(255)
+    movieInfo VARCHAR(255),
+    releaseDate VARCHAR(255),
+    posterPath VARCHAR(255)
 );
